612fd678ab84dda0184ae3ff326d2c1c73ac8933
Stéphane Vanberg <vanbergstephane@gmail.com>
2018-12-29 12:27:12 +0100

    Updates

---
+++ precache-manifest.697a732268015675bedcfb28ab14ce06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "430494a525b4c004bdc3",
+    "url": "/moon/static/js/runtime~main.430494a5.js"
+  },
+  {
+    "revision": "8e7b97b789db953a2dfc",
+    "url": "/moon/static/js/main.8e7b97b7.chunk.js"
+  },
+  {
+    "revision": "4015f97f3520c8c95b23",
+    "url": "/moon/static/js/1.4015f97f.chunk.js"
+  },
+  {
+    "revision": "8e7b97b789db953a2dfc",
+    "url": "/moon/static/css/main.419da9c9.chunk.css"
+  },
+  {
+    "revision": "4015f97f3520c8c95b23",
+    "url": "/moon/static/css/1.c27231e1.chunk.css"
+  },
+  {
+    "revision": "2dd4c33039cc2ca7b8af69fe5446bc66",
+    "url": "/moon/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c1e261fe6389d82bd78b5814f033d712.js"
+  "/moon/precache-manifest.697a732268015675bedcfb28ab14ce06.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/moon/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
