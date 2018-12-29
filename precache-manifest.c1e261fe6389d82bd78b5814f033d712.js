2f84d082cb29bbffa164b7602d8b790b8414cf3b
Stéphane Vanberg <vanbergstephane@gmail.com>
2018-12-29 12:19:27 +0100

    Updates

---
+++ precache-manifest.c1e261fe6389d82bd78b5814f033d712.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8e7b97b789db953a2dfc",
+    "url": "/static/js/main.8e7b97b7.chunk.js"
+  },
+  {
+    "revision": "4015f97f3520c8c95b23",
+    "url": "/static/js/1.4015f97f.chunk.js"
+  },
+  {
+    "revision": "8e7b97b789db953a2dfc",
+    "url": "/static/css/main.419da9c9.chunk.css"
+  },
+  {
+    "revision": "4015f97f3520c8c95b23",
+    "url": "/static/css/1.c27231e1.chunk.css"
+  },
+  {
+    "revision": "06bc29000fa5f94bcc808d748edf0512",
+    "url": "/index.html"
+  }
+];
